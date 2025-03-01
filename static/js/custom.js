34a7735560ec8bf0df4d4da6c42db645a8327384
minnanosaiban <skillupmu@gmail.com>
2025-03-01 23:06:37 +0900

    a

--- static/js/custom.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('.share-button').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.open(this.href, '_blank', 'width=600,height=400');
-  });
-});
